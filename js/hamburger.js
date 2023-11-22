e307a596d488cd82afeb3a79b3bc543dfd1e7e49
jcgamboa <jcgamboa@umich.edu>
2023-11-21 23:36:23 -0500

    Initialized Repo

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+function openSideMenu() {
+    const sideMenu = document.querySelector('.side-menu');
+    sideMenu.style.display = 'flex';
+}
+function closeMenu() {
+    const sideMenu = document.querySelector('.side-menu');
+    sideMenu.style.display = 'none';
+}
